897e8b2dc77b6ea4278ab9ebeb9c9371d9b0746a
mikespriggs26 <mikespriggs@msn.com>
2017-02-28 07:36:13 -0500

    VirtualPet

--- VirtualPet_3/Program.cs
+++ VirtualPet_3/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
+using System.Timers;
+
 
 namespace VirtualPet_3
 {
@@ -10,6 +13,80 @@ namespace VirtualPet_3
     {
         static void Main(string[] args)
         {
+            VirtualPet Ranger = new VirtualPet();
+
+
+            {   //begin user interaction
+                Console.WriteLine("Ranger's current health stats are: ");
+                Ranger.DogInfo();
+
+                List<string> careOptions = new List<string>();
+                careOptions.Add("1. Feed");
+                careOptions.Add("2. Water");
+                careOptions.Add("3. Give Insulin shot");
+
+                for (int i = 0; i < 5; i++) //loop will print options, read input, and adjust variables accordingly
+                {
+                    foreach (string option in careOptions)
+                    {
+                        Console.WriteLine(option);
+                    }
+
+                    Console.WriteLine("Enter the number for the task you want to perform. Press any other key to quit.");
+                    string userChoice = Console.ReadLine();
+                    if (userChoice == "1")
+                    {
+                        Ranger.FeedDog();   //will adjust stats and
+                        Ranger.DogInfo();   //print updated stats
+                        Console.WriteLine("=========================");  //divider for improved user readability
+                    }
+                    else if (userChoice == "2")
+                    {
+                        Ranger.WaterDog();
+                        Ranger.DogInfo();
+                        Console.WriteLine("=========================");
+                    }
+                    else if (userChoice == "3")
+                    {
+                        Ranger.GiveInsulin();
+                        Ranger.DogInfo();
+                        Console.WriteLine("=========================");
+                    }
+                    else
+                    {
+                        Environment.Exit(0);
+                    }
+                }
+
+            }
+
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ VirtualPet_3/VirtualPet.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet_3
+{
+    class VirtualPet
+    {
+        private int hunger;
+        private int water;
+        private int insulin;
+
+        public VirtualPet()
+        {
+            this.hunger = 30;
+            this.water = 20;
+            this.insulin = 40;
+        }
+
+
+        public int Hunger
+        {
+            get { return this.hunger; }
+            set { this.hunger = value; }
+        }
+
+        public int Waste
+        {
+            get { return this.water; }
+            set { this.water = value; }
+        }
+
+        public int Insulin
+        {
+            get { return this.insulin; }
+            set { this.insulin = value; }
+        }
+        public void DogInfo()
+        {
+            Console.WriteLine("Hunger: " + this.hunger);
+            Console.WriteLine("Water: " + this.water);
+            Console.WriteLine("Insulin: " + this.insulin);
+        }
+
+        public void FeedDog()
+        {
+            hunger += 10;
+            water -= 10;
+            insulin -= 10;
+            Console.WriteLine("Ranger's new stats are:");
+        }
+        public void WaterDog()
+        {
+            water += 10;
+            hunger -= 10;
+            insulin -= 10;
+            Console.WriteLine("Ranger's new stats are:");
+        }
+        public void GiveInsulin()
+        {
+            insulin += 10;
+            water -= 10;
+            hunger -= 10;
+            Console.WriteLine("Ranger's new stats are:");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
